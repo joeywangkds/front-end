0cd6c85e62ad53f2adf85eafbbc07a6dd43a35f1
joeywangkds <spiderkds@gmail.com>
2022-08-16 18:31:27 +1000

    0816

--- ch-3/Practise/pracise1.php
+++ ch-3/Practise/pracise1.php
@@ -5,11 +5,35 @@ class Demo1{
 
      public function __get($name)
      {
-        if(array_key_exists($name,$this.data->name)){
-            
-        }
+        // if(array_key_exists($name, $this->data)){
+        //     return $this->data['age'];
+        // }else{
+        //     return "age is not exist";
+        // }
+        array_key_exists($name, $this->data)?$this->data['age']:"age is not exist"; 
      }
  
+     public function getUserInfo($name, $arguments)
+     {
+        # code...
+     }
 
+
+     public function __call($name, $arguments)
+     {
+        printf('The function name is : %s, and the info is <pre>%s</pre>',$name,$arguments);
+        var_dump($arguments);
+        
+     }
 
 }
+
+$obj = new Demo1();
+// echo $obj->age;
+
+$obj2 = new Demo1();
+
+$obj2->hello(20);
+
+
+
