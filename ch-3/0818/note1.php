436f005283cc25bc36517f5846ccf412172804ad
joeywangkds <spiderkds@gmail.com>
2022-08-22 13:09:47 +1000

    -822

--- ch-3/0818/note1.php
+++ ch-3/0818/note1.php
@@ -2,18 +2,44 @@
 
 // printf('<pre>%s</pre>',print_r($_FILES,true));
 
-if(isset($_FILES['my_pic'])){
+if (isset($_FILES['my_pic'])) {
     $fileName = $_FILES['my_pic']['name'];
     $fileType = $_FILES['my_pic']['type'];
-    $tmpnName = $_FILES['my_pic']['tmp_name'];
+    $tmpName = $_FILES['my_pic']['tmp_name'];
     $error = $_FILES['my_pic']['error'];
     $fileSize =$_FILES['my_pic']['size'];
 
-    if($error!=0){
+    if ($error==0) {
         echo "Submit successful!<br>";
+if (is_uploaded_file($tmpName)) {
+    $allow = ['jpg','jpeg','png','gif'];
+    $ext = pathinfo($fileName)['extension'];
+
+    if (in_array($ext, $allow)) {
+        $path = 'upload/';
+
+        $dest = md5($fileName).'.'.$ext;
+
+        if (move_uploaded_file($tmpName, $dest)) {
+            echo 'upload success';
+        } else {
+            echo 'not success';
+        }
+    } else {
+        echo 'The file extension is not support';
     }
-    
-    echo 'The file name is '. $fileName;
+}else{
+    echo 'Upload method is wrong';
+}
+    }else {
+        switch($error) {
+            case 1:
+                echo 'The size is larger than php.ini maximum upload size';
+                break;
+            case 2:
+                echo 'The size is larger than (MAX_FILE_SIZE)) maximum upload size';
+        }
+    }
 }
 
 ?>
@@ -29,7 +55,7 @@ if(isset($_FILES['my_pic'])){
     <form action="" method="POST" enctype="multipart/form-data">
         <fieldset>
             <legend>Upload</legend>
-            <input type="hidden" name="MAX_FILE_SIZE" value="10000">
+            <!-- <input type="hidden" name="MAX_FILE_SIZE" value="10000"> -->
             <input type="file" name ="my_pic">
             <button>submit</button>
         </fieldset>
